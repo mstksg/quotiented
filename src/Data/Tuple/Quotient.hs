dd1d092a8b3a55f3c8db555c18aac971a1a04c76
Justin Le <justin@jle.im>
2016-01-23 01:22:17 -0800

    cycles

--- src/Data/Tuple/Quotient.hs
+++ src/Data/Tuple/Quotient.hs
@@ -9,22 +9,22 @@ import Data.Proxy
 import Data.Quotient
 import Data.List
 
-data Unordered
+data Permutation
 
-instance Ord a => Equiv Unordered (a, a) where
-    type EquivClass Unordered (a, a) = (a, a)
+instance Ord a => Equiv Permutation (a, a) where
+    type EquivClass Permutation (a, a) = (a, a)
 
-    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a) :/ Unordered))
+    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a) :/ Permutation))
     fromEquivClass _ = id
 
     normalizeEquivClass _ (x, y)
       | x <= y    = (x, y)
       | otherwise = (y, x)
 
-instance Ord a => Equiv Unordered (a, a, a) where
-    type EquivClass Unordered (a, a, a) = (a, a, a)
+instance Ord a => Equiv Permutation (a, a, a) where
+    type EquivClass Permutation (a, a, a) = (a, a, a)
 
-    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a, a) :/ Unordered))
+    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a, a) :/ Permutation))
     fromEquivClass _ = id
 
     normalizeEquivClass _ (x, y, z)
@@ -32,13 +32,48 @@ instance Ord a => Equiv Unordered (a, a, a) where
           [x',y',z'] -> (x', y', z')
           _          -> error "Unexpected result of sort"
 
-instance Ord a => Equiv Unordered (a, a, a, a) where
-    type EquivClass Unordered (a, a, a, a) = (a, a, a, a)
+instance Ord a => Equiv Permutation (a, a, a, a) where
+    type EquivClass Permutation (a, a, a, a) = (a, a, a, a)
 
-    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a, a, a) :/ Unordered))
+    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a, a, a) :/ Permutation))
     fromEquivClass _ = id
 
     normalizeEquivClass _ (x, y, z, r)
       = case sort [x,y,z,r] of
           [x',y',z',r'] -> (x', y', z', r')
           _             -> error "Unexpected result of sort"
+
+data Cycle
+
+instance Ord a => Equiv Cycle (a, a) where
+    type EquivClass Cycle (a, a) = (a, a)
+
+    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a) :/ Cycle))
+    fromEquivClass _ = id
+
+    normalizeEquivClass _ (x, y)
+      | x <= y    = (x, y)
+      | otherwise = (y, x)
+
+instance Ord a => Equiv Cycle (a, a, a) where
+    type EquivClass Cycle (a, a, a) = (a, a, a)
+
+    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a, a) :/ Cycle))
+    fromEquivClass _ = id
+
+    normalizeEquivClass _ (x, y, z)
+      | x <= y && x <= z = (x, y, z)
+      | y <= x && y <= z = (y, z, x)
+      | otherwise        = (z, x, y)
+
+instance Ord a => Equiv Cycle (a, a, a, a) where
+    type EquivClass Cycle (a, a, a, a) = (a, a, a, a)
+
+    toEquivClass _ = normalizeEquivClass (Proxy :: Proxy ((a, a, a, a) :/ Cycle))
+    fromEquivClass _ = id
+
+    normalizeEquivClass _ (x, y, z, r)
+      | x <= y && x <= z && x <= r = (x, y, z, r)
+      | y <= x && y <= z && y <= r = (y, z, r, x)
+      | z <= x && z <= y && z <= r = (z, r, x, y)
+      | otherwise                  = (r, x, y, z)
